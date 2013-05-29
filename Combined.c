e453ed21235f1d1287b36a73626fb7d5a7ffae71
foxton <foxx89@hotmail.com>
2013-05-29 12:07:56 +0200

    Combined calculator 0.0.5

--- Combined.c
+++ Combined.c
@@ -3,40 +3,46 @@ int main()
 {
 	int selection;
 	float resistance, voltage, current;
-	printf("-COMBINED CALCULATOR-\n");
+	printf("-----COMBINED CALCULATOR-----\n");
+	printf("A simple program for calculating voltage,\n");
+	printf("resistance and current in electric circuits.\n\n");
+	do
+    {
 	printf("--------MENU--------\n");
 	printf("1.Calculate Voltage\n");
 	printf("2.Calculate Resistance\n");
 	printf("3.Calculate Current\n");
+	printf("4.Exit\n");
 	printf("Selection:");
 	scanf("%d", &selection);
 	switch (selection)
 	{
 	case 1:
-		
-		printf("Enter Resistance:");
-    scanf("%d", &resistance);
+
+		printf("\nEnter Resistance:");
+    scanf("%f", &resistance);
 	if (resistance < 0)
 	{
 		resistance = -resistance;
 	}
 	fflush(stdin);
 	printf("Enter Current:");
-    scanf("%d", &current);
+    scanf("%f", &current);
 	if (current < 0)
 	{
 		current = -current;
 	}
     fflush(stdin);
     voltage = resistance * current;
-    printf("Voltage = %d", voltage);
+    printf("Voltage = %.2f", voltage);
     printf("Volt\n");
+    printf("Press any key to return\n");
 	getchar();
 		break;
 
 	case 2:
-		
-		printf("Enter Voltage:");
+
+		printf("\nEnter Voltage:");
 	scanf("%f", &voltage);
 	if(voltage < 0)
 	{
@@ -54,11 +60,12 @@ int main()
 	resistance = voltage / current;
 	printf("Resistance = %.2f", resistance);
 	printf("Ohm\n");
+	printf("Press any key to return\n");
 	getchar();
 		break;
 	case 3:
-	
-	printf("Enter voltage: ");
+
+	printf("\nEnter voltage: ");
     scanf("%f", &voltage);
     fflush(stdin);
 	if (voltage <0)   // Check if voltage is negative
@@ -75,12 +82,21 @@ int main()
      current = voltage / resistance;  //Calculate the total current
      printf("Current = %.2f",current);
      printf("Amp\n");
+     printf("Press any key to return\n");
 	 getchar();
 		break;
-	default:
+
+    case 4:
+        printf("Program shutting down");
+        return 0;
+        break;
+
+        default:
+            //printf("Unvalid option. Enter a new number: ");
 		printf("Error, shutting down");
+		return 0;
 		break;
-getchar();
-return 0;
+
 	}
+} while (selection != 4);
 }
